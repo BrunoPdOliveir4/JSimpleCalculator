28ee73aca2b4358dcf80d0922d6110764ee2469c
Bruno Pedroso <95253379+BrunoPdOliveir4@users.noreply.github.com>
2022-03-21 16:14:46 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+function add(text) {
+    var addtext = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML= addtext+text;
+}
+function clean() {
+    document.getElementById('result').innerHTML = '';
+    var addtext = null;
+}
+function delt() {
+    var backs = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = backs.substring(0, backs.length -1)
+}
+function equal() {
+    var result = document.getElementById('result').innerHTML;
+    if(result)
+    {
+        document.getElementById('result').innerHTML = eval(result)
+    }
+    else
+    {
+        window.alert('Nada para calcular!')
+    }
+}
